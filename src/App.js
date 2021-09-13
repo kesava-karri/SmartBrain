080c552940a99db48a358b0e6e7d3a40b8a16545
Kesava Karri <kkeshav1998@gmail.com>
2021-09-12 23:12:05 -0400

    added route & signout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation.js';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank.js';
@@ -10,7 +12,7 @@ import './App.css';
 
 const app = new Clarifai.App({
   apiKey: '0ef836f6ea24435bb2218505c68c5975',
-})
+});
 
 const particlesOptions = {
   particles: {
@@ -29,7 +31,10 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}, // will contain the values of the box around face
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -37,35 +42,65 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      topRow: clarifaiFace.topRow * height, // here clarifaiFace.topRow gives the percent of total image topRow is at
+      leftCol: clarifaiFace.leftCol * width,
+      bottomRow: height - (clarifaiFace.bottomRow * height),
+      rightCol: width - (clarifaiFace.rightCol * width)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict(
       "c0c0ac362b03416da06ab3fa36fb58e3", //Clarifai.FACE_DETECT_MODEL
       this.state.input) // can't use imageUrl -> gives bad request cause that's how setState works
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // tehre was an error
-      }
-    )
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state; //destructuing
     return(
       <div className='App'>
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <Register onRouteChange={this.onRouteChange}/>
+            )
+        }
       </div>
     )
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return(
     <div className='center ma'>
       <div className='absolute mt2'></div>
-      <img alt='' src={imageUrl} width='500px' height='auto'/>
+      <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+      <div className='bouding-box' style={{top: box.topRow, left: box.leftCol, bottom: box.bottomRow, right: box.rightCol}}></div>
     </div>
   )
 }
